93d6d075bbe68aa80b43b1609b074dcfc7372112
marcos <marcosmasci@gmail.com>
2020-09-14 15:30:39 -0300

    refactor: setup and __init__.py

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-from setuptools import Extension, setup
+from setuptools import setup
 from Cython.Build import cythonize
-from Cython.Distutils import build_ext
-
-ext_modules = [
-    Extension("PyTwoPlusTwoEval", ["twoplustwo_eval/evaluate.pyx"], include_dirs=['.']),
-]
 
 setup(
-    name='PyTwoPlusTwoEval',
+    name='twoplustwo_eval',
     version='1.0',
     packages=['twoplustwo_eval'],
     url='',
     license='',
     author='marcos',
     author_email='',
-    description='',
-    ext_modules=cythonize(ext_modules, compiler_directives={'language_level': "3"}),
-    cmdclass={'build_ext': build_ext},
-    script_args=['build_ext'],
-    options={'build_ext': {'inplace': True, 'force': False}}
+    description='evaluate poker hands',
+    ext_modules=cythonize('twoplustwo_eval/*.pyx'),
+    install_requires=['cython', 'numpy', 'future'],
 )
