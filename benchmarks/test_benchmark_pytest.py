a6430870aaa83565d60a3490f8d8fc6f4ea33c8e
Marcos Masci <marcosmasci@gmail.com>
2026-03-09 18:25:18 +0000

    test: new benchmarks

--- benchmarks/test_benchmark_pytest.py
+++ benchmarks/test_benchmark_pytest.py
@@ -1,7 +1,5 @@
 import pytest
 
-import pytest
-
 # Salta benchmarks si no está instalado el plugin pytest-benchmark
 pytest.importorskip("pytest_benchmark")
 
@@ -9,6 +7,7 @@ from poker_eval_faster import (
     evaluate_hands,
     evaluate_one_hand_vs_all,
     evaluate_rank,
+    evaluate_ranges
 )
 
 
@@ -17,6 +16,18 @@ def test_benchmark_evaluate_hands(benchmark):
     hands = [['9c', '8c'], ['Tc', 'Td']]
     board = ['Qh', 'Jh', '8s']
     benchmark(lambda: evaluate_hands(hands, board))
+
+
+@pytest.mark.benchmark(group="evaluate_two_combos_preflop")
+def test_benchmark_evaluate_two_combos_preflop(benchmark):
+    hands = [['Ac', 'Kc'], ['2d', '2h']]
+    benchmark(lambda: evaluate_hands(hands))
+
+
+@pytest.mark.benchmark(group="evaluate_tree_combos_preflop")
+def test_benchmark_evaluate_tree_combos_preflop(benchmark):
+    hands = [['Ac', 'Kc'], ['2d', '2h'], ['5s', '6s']]
+    benchmark(lambda: evaluate_hands(hands))
 
 
 @pytest.mark.benchmark(group="evaluate_one_vs_all")
@@ -31,3 +42,8 @@ def test_benchmark_evaluate_rank(benchmark):
     hand = ['Kh', 'Ah']
     board = ['Th', 'Jh', 'Qh']
     benchmark(lambda: evaluate_rank(board, hand))
+
+
+@pytest.mark.benchmark(group="evaluate_simple_ranges_preflop")
+def test_benchmark_evaluate_simple_ranges_preflop(benchmark):
+    benchmark(lambda: evaluate_ranges("AA", "KK"))
--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,52 +1,7 @@
-from poker_eval_faster import evaluate_hands, evaluate_one_hand_vs_all, distribution_one_hand_vs_all, evaluate_rank
-from poker_eval_faster import ranking_to_category
-import pytest
-from typing import cast
-
-ps_hand_vs_all = [
-    [['5c', '2d'], ['2c', '2h', '5h', '7h', '7s'], 88.99],
-    [['Qc', '2d'], ['5c', 'Tc', '3d', '4d', '9h'],  12.17],
-    [['Ac', '4d'], ['8c', '4h', '5h', 'Jh', '2s'],  50.61],
-    [['4c', '5d'], ['2c', '7c', 'Qc', 'Qh', '6s'],  2.22],
-    [['6c', '8d'], ['9c', '2h', 'Th', '2s', '9s'],  10.15],
-    [['6c', '6d'], ['9c', '7d', 'Jh', 'Ah', '6s'],  95.56],
-    [['9c', 'Jc'], ['5d', '9d', 'Kd', '4h'],  65.48],
-    [['3c', '4c'], ['6c', '6d', 'Kd', '7h'],  19.37],
-    [['Jc', '3d'], ['Ac', '9d', '7h', 'Qh'],  31.12],
-    [['6c', 'Kd'], ['3c', 'Qc', '5h', '7h'],  37.3],
-    [['Tc', '9d'], ['2c', 'Ac', 'Qh', '7s'],  28.87],
-    [['5c', '8d'], ['8c', 'Tc', '3h', 'Jh'],  57.26],
-    [['Tc', 'Qc'], ['Ad', 'Jh', '3s'], 52.4],
-    [['8c', '6d'], ['9c', '5d', '3h'], 36.16],
-    [['5c', '9d'], ['9c', 'Qc', 'Jh'], 58.99],
-    [['7c', 'Kc'], ['7d', 'Jd', '6h'], 67.11],
-    [['5c', '7d'], ['Ac', '5h', '7h'], 82.59],
-    [['Kc', 'Jd'], ['2c', '4c', '2d'], 52.19],
-]
-
-ps_eval_hands = [
-    [[['9c', '8c'], ['Tc', 'Td']], ['Qh', 'Jh', '8s'], [15.45, 84.55]],
-    [[['Kc', 'Qc'], ['6c', '6d'], ['9s', '8s']], [], [41.25, 28.18, 30.57]],
-    [[['Kc', 'Qc'], ['6c', '6d'], ['Kd', 'Kh']], [], [12.85, 19.87, 67.27]]
-    ]
+from poker_eval_faster import evaluate_hands
 
 
-@pytest.mark.skip(reason="migrado a test_eval_one_hand_vs_all_py.py")
-def test_placeholder_migrated_1():
-    assert True
+def test_evaluate_hands():
+    r = evaluate_hands([["2c", "2c"], ["2c", "2c"]], ["2c", "2c", "2c"])
 
-
-@pytest.mark.skip(reason="migrado a test_distribution.py")
-def test_placeholder_migrated_2():
-    assert True
-
-
-@pytest.mark.skip(reason="migrado a test_evaluate_hands_c.py y wrappers")
-def test_placeholder_migrated_3():
-    assert True
-
-
-@pytest.mark.skip(reason="migrado a test_evaluate_rank.py")
-def test_placeholder_migrated_4():
-    assert True
 
