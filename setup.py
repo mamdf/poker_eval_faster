61a4d3309d220cffe4f36f4e1fe61b67e7a5f128
Marcos Masci <marcosmasci@gmail.com>
2026-03-19 18:51:14 -0300

    Moderniza empaquetado y agrega CI

--- setup.py
+++ setup.py
@@ -1,23 +1,73 @@
-# primero instalar Cython para ejecutar este setup.py
-# para compilar los C y poder testear y ejecutar desde Pycharm: python setup.py build_ext --inplace
+from pathlib import Path
 
-from setuptools import setup
-from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+ROOT = Path(__file__).parent
+README = (ROOT / "README.md").read_text(encoding="utf-8")
+EXTENSION_NAMES = (
+    "common",
+    "hands_evaluate",
+    "main",
+    "one_hand_evaluate",
+    "python_wrapper",
+)
+USE_CYTHON = cythonize is not None
+SOURCE_SUFFIX = ".pyx" if USE_CYTHON else ".c"
+
+
+def build_extensions() -> list[Extension]:
+    extensions = [
+        Extension(
+            name=f"poker_eval_faster.eval_cython.{extension_name}",
+            sources=[f"poker_eval_faster/eval_cython/{extension_name}{SOURCE_SUFFIX}"],
+        )
+        for extension_name in EXTENSION_NAMES
+    ]
+    if not USE_CYTHON:
+        return extensions
+    return cythonize(
+        extensions,
+        compiler_directives={"language_level": "3"},
+    )
+
 
 setup(
-    name='poker_eval_faster',
-    version='1.0',
-    packages=['poker_eval_faster'],
-    url='',
-    license='',
-    author='marcos masci',
-    author_email='',
-    description='evaluate poker hands',
-    package_data={'poker_eval_faster': ['data/*']},
-    ext_modules=cythonize('poker_eval_faster/eval_cython/*.pyx', compiler_directives={'language_level': "3"}),
-    entry_points='''
-        [console_scripts]
-        poker-eval=poker_eval_faster.script:run
-    ''',
-    install_requires=['cython', 'numpy', 'future', 'pytest', 'click'],
+    name="poker_eval_faster",
+    version="1.0",
+    description="High-performance poker hand evaluation for Texas Hold'em",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="marcos masci",
+    python_requires=">=3.11",
+    packages=find_packages(include=["poker_eval_faster", "poker_eval_faster.*"]),
+    include_package_data=True,
+    package_data={
+        "poker_eval_faster": ["data/*.dat", "data/*.bin"],
+        "poker_eval_faster.eval_cython": ["*.pxd", "*.pyx", "*.c"],
+    },
+    ext_modules=build_extensions(),
+    install_requires=[
+        "click",
+        "numpy",
+    ],
+    extras_require={
+        "dev": [
+            "build",
+            "cython",
+            "pytest",
+            "pytest-benchmark",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "poker-eval=poker_eval_faster.script:run",
+        ]
+    },
+    zip_safe=False,
 )
